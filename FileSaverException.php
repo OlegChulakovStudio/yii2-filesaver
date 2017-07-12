3d44068fedec77a98a53f2d2e527319fe807777f
lilit <lb@chulakov.ru>
2017-07-12 16:44:14 +0300

    init commit

--- FileSaverException.php
+++ FileSaverException.php
@@ -2,6 +2,10 @@
 
 namespace OlegChulakovStudio\filesaver;
 
+/**
+ * Class FileSaverException
+ * @package OlegChulakovStudio\filesaver
+ */
 class FileSaverException  extends \yii\base\Exception
 {
 }
